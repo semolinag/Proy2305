e4e08a562eea7231d85f64478c9910ff472c4e22
cetecom <cetecom@LC08L1203>
2017-05-25 12:19:53 -0400

    me rindo

--- index.php
+++ index.php
@@ -1,22 +1,60 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
+<?php
+    session_start();
+?>
 <html>
     <head>
         <meta charset="UTF-8">
         <title></title>
     </head>
     <body>
+        <div>
+            <?php
+                if(isset($_SESSION["Usuario"])){
+            ?>
+            <div align="center"><button><a href="controladores/CerrarSesion.php">Cerrar Sesion</a></button></div>
+            <a href="formularios/FormIngresoProducto.php">Ingresar Producto</a><br>
+            <a href="formularios/FormActualizarClave.php">Cambiar Clave</a>
+            <?php
+                }
+            ?>
+        </div>
         <?php
-        // put your code here?>
-        <form method="post" action="modulos/prod_agregar.php">
-            <div><label>Nombre:</label><input type="text" name="nombre"></div>
-            <div><label>Usuario:</label><input type="text" name="usuario"></div>
-            <div><label>Password:</label><input type="password" name="password"></div>
-            <input type="submit">
+            if(!isset($_SESSION["Usuario"])) {
+        ?>
+        <form method="post" action="controladores/IniciarSesion.php">
+            <table>
+                <tr>
+                    <td>
+                        NOMBRE
+                    </td>
+                    <td>
+                        <input type="text" name="nombre">
+                        
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>
+                        CONTRASEÑA
+                    </td>
+                    <td>
+                        <input type="password" name="pass">                        
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <input type="submit" value="Verificar">
+                    </td>
+                </tr>
+                    
+                </tr
+            </table>
+            
+        </form>
+                
+        <?php
+            }
+        ?>
         
     </body>
 </html>
